ec851e6f248a489f994bc67c2d3a76c081ed5a3a
Brusnikina <43144646+Brusnikina@users.noreply.github.com>
2019-10-24 21:01:21 +0300

    Update ex_6.py

--- ex_6.py
+++ ex_6.py
@@ -3,10 +3,10 @@ import json
 import sys
 from librip.ctxmngrs import timer
 from librip.decorators import print_result
-from librip.gens import field, gen_random
-from librip.iterators import Unique as unique
+from librip.gen import field, gen_random
+from librip.iterators import Unique
 
-path = None
+path = sys.argv[1]
 
 # Здесь необходимо в переменную path получить
 # путь до файла, который был передан при запуске
@@ -23,22 +23,24 @@ with open(path) as f:
 
 @print_result
 def f1(arg):
-    raise NotImplemented
+    return sorted(Unique(field(arg, 'job-name'), ignore_case=True))
 
 
 @print_result
 def f2(arg):
-    raise NotImplemented
+    return list(filter(lambda x: x.lower().startswith('программист'), arg))
 
 
 @print_result
 def f3(arg):
-    raise NotImplemented
+    return list(map(lambda x: x + ' с опытом Python', arg))
 
 
 @print_result
 def f4(arg):
-    raise NotImplemented
+    salary = list(gen_random(100000, 200000, len(arg)))
+    jobs = list(zip(arg, salary))
+    return list(map(lambda x: x[0] + ', зарплата ' + str(x[1]) + ' руб.', jobs))
 
 
 with timer():
