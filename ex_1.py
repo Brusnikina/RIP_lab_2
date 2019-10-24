8001c84cc8a165acb35303d812e05c9f0e263ddf
Brusnikina <43144646+Brusnikina@users.noreply.github.com>
2019-10-24 20:55:43 +0300

    Update ex_1.py

--- ex_1.py
+++ ex_1.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-from librip.gens import field
+from liprip.gen import field
+from librip.gen import gen_random
 
 goods = [
     {'title': 'Ковер', 'price': 2000, 'color': 'green'},
     {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'},
-    {'title': 'Стелаж', 'price': 7000, 'color': 'white'},
-    {'title': 'Вешалка для одежды', 'price': 800, 'color': 'white'}
+    {'title': 'Кресло', 'price': 5000, 'color': 'grey'},
+    {'title': 'Книжная полка', 'price': 1200, 'color': 'brown'}
 ]
 
 # Реализация задания 1
+# str(...)[1:-1] - срез нач. с индекса 1 по -1 не включая его (убираем [])
+print(str(list(field(goods, 'title', 'price')))[1:-1])
+print(str(list(gen_random(1, 4, 10)))[1:-1])
